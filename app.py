e36155a603f02d2694654292159f55d5685d45a7
Yasir Mushtaq <46179498+yasir23@users.noreply.github.com>
2024-06-11 18:26:46 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+import os
+import streamlit as st
+from langchain.chains import create_retrieval_chain
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_chroma import Chroma
+import time
+import markdown
+from langchain_community.document_loaders import UnstructuredMarkdownLoader
+
+
+
+from langchain_community.document_loaders import PyPDFLoader
+
+file_path = (
+    "Law-on-Drafting-Pleading-Conveyancing-YAL.pdf"
+)
+
+
+
+
+if "vector" not in st.session_state:
+
+    st.session_state.embeddings = FastEmbedEmbeddings(model_name = 'BAAI/bge-small-en-v1.5')
+
+    st.session_state.loader = PyPDFLoader(file_path)
+    st.session_state.docs = st.session_state.loader.load_and_split()
+
+    st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=200)
+    st.session_state.documents = st.session_state.text_splitter.split_documents( st.session_state.docs)
+    st.session_state.vector = Chroma.from_documents(st.session_state.documents, st.session_state.embeddings)
+st.title("Legal drafting")
+
+
+
+llm = ChatGroq(
+    api_key = st.secrets["GROQ_API_KEY"],
+    model_name='mixtral-8x7b-32768'
+    )
+
+prompt = ChatPromptTemplate.from_template("""
+Answer the following question based only on the provided context. 
+Think step by step before providing a detailed answer. 
+I will tip you $200 if the user finds the answer helpful. 
+<context>
+{context}
+</context>
+
+Question: {input}""")
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+retriever = st.session_state.vector.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+prompt = st.text_input("Input your prompt here")
+
+
+# If the user hits enter
+if prompt:
+    # Then pass the prompt to the LLM
+    start = time.process_time()
+    response = retrieval_chain.invoke({"input": prompt})
+    print(f"Response time: {time.process_time() - start}")
+
+
+
+    st.write(response["answer"])
+
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            # print(doc)
+            # # st.write(f"Source Document # {i+1} : {doc.metadata['source'].split('/')[-1]}")
+            st.write(doc.page_content)
+            st.write("--------------------------------")
